8c8a32e98ad88135d8760ec31fda855ccf3f445f
Sacha Kozma <sacha.kozma@AA18-10.local>
2015-05-05 11:23:08 +0200

    Initial Commit

---
+++ Mastermind/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mastermind
+//
+//  Created by Sacha Kozma on 05.05.15.
+//  Copyright (c) 2015 Sacha KOZMA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
