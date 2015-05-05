52ed6a3446a79ffb0bec43e2629e67f67e3f10ae
Sacha Kozma <sacha.kozma@AA18-10.local>
2015-05-05 11:45:53 +0200

    First commit (unfinished)

--- Mastermind/main.cpp
+++ Mastermind/main.cpp
@@ -8,11 +8,36 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int combinaisonMystere [4], nombreEssai = 0, combinaisonUtilisateur[4];
+    bool combinaisonTrouvee = false;
+    
+    cout << "Bienvenue au jeu du Mastermind !" << endl << endl;
+    cout << "Génération d'une nouvelle partie ..." << endl << endl;
+    
+    for (int i = 0; i < 4; i++) {
+        combinaisonMystere[i] = random() % 8 + 1;
+    }
+    
+    
+    do{
+        nombreEssai++;
+        
+        cout << "Essai " << nombreEssai << "/10, entrer une combinaison (séparer les chiffres d'un espace : ";
+        for (int i = 0; i < 4; i++) {
+            cin >> combinaisonUtilisateur[i];
+        }
+        
+        
+        
+    }while (combinaisonTrouvee == false && nombreEssai < 10);
+    
+    
+    
+    
     return 0;
 }
-
